d3648cb5faf73d4e214023530c1062f8d5dbe8ad
Wolfolo <daniele.termini@gmail.com>
2017-08-06 17:52:10 +0200

    Implemented Prototype classes and tests

---
+++ DesignPatternsExercise/CreationalPatterns/Prototype/Prototype.cs
@@ -0,0 +1,36 @@
+﻿/// <summary>
+/// The abstract prototype defines the base properties
+/// The clone uses the base properties?
+/// </summary>
+namespace DesignPatternsExercise.CreationalPatterns.Prototype
+{
+    abstract class Prototype
+    {
+        public string String { get; set; } = "Prototype";
+        public int Number { get; set; }
+        public bool Flag { get; set; }
+
+        public object Clone()
+        {
+            Prototype clone = (Prototype)this.MemberwiseClone();
+
+            HandleClone(clone);
+
+            return clone;
+        }
+
+        protected virtual void HandleClone(Prototype clone)
+        {
+        }
+    }
+
+    class Concrete : Prototype
+    {
+        protected override void HandleClone(Prototype clone)
+        {
+            base.HandleClone(clone);
+
+            clone.String += "Clone";
+        }
+    }
+}
---
+++ DesignPatternsExercise/CreationalPatterns/Prototype/PrototypeTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignPatternsExercise.CreationalPatterns.Prototype
+{
+    [TestClass]
+    public class PrototypeTest
+    {
+        [TestMethod]
+        public void TestCreationFromPrototype()
+        {
+            // Configure the prototype
+            var prototype = new Concrete();
+            prototype.Number = 4;
+            prototype.Flag = true;
+
+            // Clone it
+            var clone = (Concrete)prototype.Clone();
+
+            Assert.AreEqual("Prototype", prototype.String);
+            Assert.AreEqual("PrototypeClone", clone.String);
+            Assert.AreEqual(prototype.Number, clone.Number);
+            Assert.AreEqual(prototype.Flag, clone.Flag);
+        }
+    }
+}
