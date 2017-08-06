6caecb3ae3d8663a35b4c96b9eb2450eeebd9a79
Wolfolo <daniele.termini@gmail.com>
2017-08-06 12:10:01 +0200

    Implemented Singleton classes and tests

---
+++ DesignPatternsExercise/CreationalPatterns/Singleton/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/// <summary>
+/// A singleton class which provides only one instance of itself
+/// </summary>
+namespace DesignPatternsExercise.CreationalPatterns.Singleton
+{
+    class Singleton
+    {
+        static Singleton instance = new Singleton();
+
+        public int Value { get; set; }
+
+        /// <summary>
+        /// A singleton class must not be instanced outside of his own
+        /// </summary>
+        private Singleton()
+        {
+        }
+
+        /// <summary>
+        /// Returns the single instance for this class
+        /// </summary>
+        /// <returns>Always the same Singleton instance</returns>
+        static public Singleton Instance()
+        {
+            return instance;
+        }
+    }
+}
---
+++ DesignPatternsExercise/CreationalPatterns/Singleton/SingletonTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+using System.Threading;
+
+namespace DesignPatternsExercise.CreationalPatterns.Singleton
+{
+    [TestClass]
+    public class SingletonTest
+    {
+        [TestMethod]
+        public void TestCannotInstanceWithNew()
+        {
+            ConstructorInfo[] constructors = typeof(Singleton).GetConstructors();
+
+            Assert.IsTrue(constructors.Length == 0);
+        }
+
+        [TestMethod]
+        public void TestIsSameInstance()
+        {
+            var instance = Singleton.Instance();
+            var newInstance = Singleton.Instance();
+
+            Assert.AreSame(instance, newInstance);
+        }
+
+        [TestMethod]
+        public void TestChangeValue()
+        {
+            var instance = Singleton.Instance();
+            var newInstance = Singleton.Instance();
+
+            instance.Value = 4;
+
+            Assert.AreEqual(instance.Value, newInstance.Value);
+        }
+    }
+}
