db8482c6d1b4bc220c2927846fd5c2a9bad92e1a
Wolfolo <daniele.termini@gmail.com>
2017-08-09 17:53:48 +0200

    Implemented LazyInitialization classes and test

---
+++ DesignPatternsExercise/CreationalPatterns/LazyInitialization/LazyInitializationTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignPatternsExercise.CreationalPatterns.LazyInitialization
+{
+    [TestClass]
+    public class LazyInitializationTest
+    {
+        [TestCleanup]
+        public void Cleanup()
+        {
+            /* Cleanup the static object which will preserve his state globally between tests */
+            Product._Cleanup();
+        }
+
+        [TestMethod]
+        public void TestNoProductsAtStart()
+        {
+            Assert.AreEqual(0, Product.GetProducts().Count);
+        }
+
+        [TestMethod]
+        public void TestProductCreation()
+        {
+            Assert.AreEqual("foo", Product.GetProduct("foo").Name);
+            Assert.AreEqual("bar", Product.GetProduct("bar").Name);
+            Assert.AreEqual(2, Product.GetProducts().Count);
+        }
+
+        [TestMethod]
+        public void TestRequestingSameProduct()
+        {
+            var first = Product.GetProduct("foo");
+            var second = Product.GetProduct("foo");
+
+            Assert.AreSame(first, second);
+            Assert.AreEqual(1, Product.GetProducts().Count);
+        }
+    }
+}
---
+++ DesignPatternsExercise/CreationalPatterns/LazyInitialization/Product.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatternsExercise.CreationalPatterns.LazyInitialization
+{
+    class Product
+    {
+        public string Name { get; private set; }
+
+        private static Dictionary<string, Product> products = new Dictionary<string, Product>();
+
+        private Product(string name)
+        {
+            Name = name;
+        }
+
+        public static Product GetProduct(string name)
+        {
+            if (products.ContainsKey(name))
+            {
+                return products[name];
+            }
+
+            var product = new Product(name);
+
+            products.Add(name, product);
+
+            return product;
+        }
+
+        public static List<Product> GetProducts()
+        {
+            var list = new List<Product>();
+
+            foreach (Product p in products.Values)
+            {
+                list.Add(p);
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// This method is for testing only, since this is a global object and needs to be clean up
+        /// </summary>
+        public static void _Cleanup()
+        {
+            products.Clear();
+        }
+    }
+
+}
